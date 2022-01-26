626731bbe23396e8695be6ed7bad09b9ed1f262f
Adrian Petersson <adriantimothypetersson@gmail.com>
2022-01-26 22:03:54 +0100

    converted darkMode function to custom hook

---
+++ hooks/useDarkMode.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+function useDarkMode() {
+  const [theme, setTheme] = useState('dark');
+
+  const setMode = (mode) => {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('theme', mode);
+      setTheme(mode);
+    }
+  };
+  const toggleTheme = () => {
+    if (theme === 'dark') return setMode('light');
+    return setMode('dark');
+  };
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem('theme');
+    if (localTheme) return setTheme(localTheme);
+    return setMode('light');
+  }, [theme]);
+
+  return [theme, toggleTheme];
+}
+
+export default useDarkMode;
