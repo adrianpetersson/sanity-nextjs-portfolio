80e388614d32f2b5e9bb050865bf56ae9abf95d8
Adrian Petersson <adriantimothypetersson@gmail.com>
2022-01-30 23:09:15 +0100

    fixed lint-errors

--- components/StackSection.jsx
+++ components/StackSection.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/jsx-no-constructed-context-values */
 import React from 'react';
 import {
