8c2ee9919e30d9629fb8bbc2e5318c2cbc35a41b
Adrian Petersson <adriantimothypetersson@gmail.com>
2022-02-08 21:32:28 +0100

    cleanup sanity schema

--- sanity-portfolio/schemas/project.js
+++ sanity-portfolio/schemas/project.js
@@ -1,62 +1,64 @@
+/* eslint-disable import/no-anonymous-default-export */
 export default {
-  name: "project",
-  title: "Project",
-  type: "document",
+  name: 'project',
+  title: 'Project',
+  type: 'document',
   fields: [
     {
-      name: "title",
-      title: "Title",
-      type: "string",
+      name: 'title',
+      title: 'Title',
+      type: 'string',
     },
     {
-      name: "mainImage",
-      title: "Main Image",
-      type: "image",
+      name: 'mainImage',
+      title: 'Main Image',
+      type: 'image',
       options: {
         hotspot: true,
       },
     },
     {
-      name: "type",
-      title: "type",
-      type: "string",
+      name: 'type',
+      title: 'Type',
+      type: 'string',
       options: {
         list: [
-          { title: "UX", value: "ux" },
-          { title: "Frontend", value: "frontend" },
-          { title: "Other", value: "other" },
+          { title: 'UX', value: 'ux' },
+          { title: 'Frontend', value: 'frontend' },
+          { title: 'Other', value: 'other' },
         ],
-        layout: "radio",
+        layout: 'radio',
       },
     },
+    { name: 'year', title: 'Year', type: 'number' },
     {
-      name: "description",
-      title: "Description",
-      type: "string",
+      name: 'description',
+      title: 'Description',
+      type: 'string',
     },
     {
-      name: "link",
-      title: "External link",
-      type: "url",
+      name: 'link',
+      title: 'External link',
+      type: 'url',
     },
     {
-      name: "github",
-      title: "Github Link",
-      type: "url",
+      name: 'github',
+      title: 'Github Link',
+      type: 'url',
     },
     {
-      name: "slug",
-      title: "Slug",
-      type: "slug",
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
       options: {
-        source: "title",
+        source: 'title',
         maxLength: 100,
       },
     },
     {
-      name: "id",
-      title: "ID",
-      type: "number",
+      name: 'id',
+      title: 'ID',
+      type: 'number',
     },
     {
       name: 'body',
@@ -64,10 +66,10 @@ export default {
       type: 'blockContent',
     },
     {
-    name: 'imagesGallery',
-    title: 'Images gallery',
-    type: 'array',
-    of: [{ type: 'image' }]
-   }
+      name: 'imagesGallery',
+      title: 'Images gallery',
+      type: 'array',
+      of: [{ type: 'image' }],
+    },
   ],
 };
